7a945d9b58cde8344cd6e4cdbc772951377733e2
igorqualicore <carvalho.igor.isc@gmail.com>
2026-03-26 14:49:56 -0300

    Ajustes gerais na pipe.

--- cypress/page-objects/blogHomePage.js
+++ cypress/page-objects/blogHomePage.js
@@ -1,41 +1,14 @@
 class BlogHomePage {
-  selectors = {
-    searchField: '#search-field',
-    searchForm: 'form.search-form',
-  };
-
   open() {
     cy.visit('/');
   }
 
-  getSearchForm() {
-    return cy.get(this.selectors.searchForm)
-      .first()
-      .should('have.attr', 'method', 'get');
-  }
-
-  getSearchField() {
-    return this.getSearchForm()
-      .find(this.selectors.searchField)
-      .first()
-      .should('have.attr', 'name', 's');
-  }
-
-  fillSearchField(term) {
-    return this.getSearchField().then(($field) => {
-      $field.val(term);
-
-      return cy.wrap($field).should('have.value', term);
-    });
-  }
-
-  submitSearch() {
-    return this.getSearchForm().submit();
+  buildSearchPath(term) {
+    return `/?s=${encodeURIComponent(term)}`;
   }
 
   searchFor(term) {
-    this.fillSearchField(term);
-    return this.submitSearch();
+    return cy.visit(this.buildSearchPath(term));
   }
 }
 
